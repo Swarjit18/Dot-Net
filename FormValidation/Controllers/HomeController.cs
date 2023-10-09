01e4b67d4cdc26461a3f3292dedd45abd973efbd
Swarjit Sarkar <swarjitsarkar@gmail.com>
2023-10-09 20:19:59 +0600

    Form validation

---
+++ FormValidation/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using FormValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+       
+        [HttpGet]
+        public ActionResult Signup()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(Signup s)
+        {
+
+            return View(s);
+        }
+        public ActionResult SignupSubmit()
+        {
+
+
+            TempData["msg"] = "Signup Successfull";
+
+            return RedirectToAction("Index", "Dashboard");
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ FormValidation/Models/Signup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormValidation.Models
+{
+    public class Signup
+    {
+
+        public string name { get; set; }
+        public string userid { get; set; }
+        public string password { get; set; }
+        public string studentID { get; set; }
+        public string email { get; set; }
+        public string dob { get; set; }
+
+
+
+
+
+    }
+}
